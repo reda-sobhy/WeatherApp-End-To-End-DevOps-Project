81752d1cc64cf4913a41549457da7898f44207f7
reda <redabokka@gmail.com>
2026-07-28 15:37:19 +0300

    arrange the files

--- project_run.sh
+++ project_run.sh
@@ -18,37 +18,30 @@ echo "======================================"
 echo "Installing NGINX Ingress Controller"
 echo "======================================"
 
-chmod +x install-nginx-ingress.sh
-./install-nginx-ingress.sh
+aws eks update-kubeconfig --name my-eks
+kubectl create ns frontend
+kubectl create ns backend
+
+../ingress-controller/install-nginx-ingress.sh
 
 echo "======================================"
 echo "Installing External Secrets Operator"
 echo "======================================"
 
-chmod +x install-external-secret-operator.sh
-./install-external-secret-operator.sh
-
-echo "======================================"
-echo "Creating External Secrets Service Account"
-echo "======================================"
-
-chmod +x serviceaccount_external_secret.sh
-./serviceaccount_external_secret.sh
+../secret_manager/install-external-secret-operator.sh
+../secret_manager/serviceaccount_external_secret.sh
 
 echo "======================================"
 echo "Installing CloudWatch Addons"
 echo "======================================"
 
 chmod +x addons-cloudwathch.sh
-./addons-cloudwathch.sh
+../cloudwatch/addons-cloudwathch.sh
 
 echo "======================================"
 echo "Creating RDS CloudWatch Alarms"
 echo "======================================"
 
 chmod +x rds_alarm.sh
-./rds_alarm.sh
+../cloudwatch/rds_alarm.sh
 
-echo "======================================"
-echo "Deployment Completed Successfully!"
-echo "======================================"
